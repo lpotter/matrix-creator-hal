2b320276d02bd1bbb0d0812a54c5a01b06ed8d0a
Lorn Potter <lorn.potter@canonical.com>
2016-08-23 09:29:55 +1000

    add temperature and humidity demo

---
+++ demos/temperature_demo.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 <Admobilize>
+ * MATRIX Labs  [http://creator.matrix.one]
+ * This file is part of MATRIX Creator HAL
+ *
+ * MATRIX Creator HAL is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <unistd.h>
+#include <iostream>
+
+#include "../cpp/driver/everloop_image.h"
+#include "../cpp/driver/everloop.h"
+#include "../cpp/driver/humidity_data.h"
+#include "../cpp/driver/humidity_sensor.h"
+#include "../cpp/driver/wishbone_bus.h"
+
+namespace hal = matrix_hal;
+
+int main() {
+  hal::WishboneBus* bus = new hal::WishboneBus();
+  bus->SpiInit();
+
+  hal::HumiditySensor humidity_sensor;
+  humidity_sensor.Setup(bus);
+
+  hal::HumidityData humidity_data;
+  humidity_sensor.Read(&humidity_data);
+
+    std::cout << "humidity = " << humidity_data.humidity << "\t";
+    std::cout << "temperature = " << humidity_data.temperature << std::endl;
+    usleep(200000);
+  }
+
+  return 0;
+}
